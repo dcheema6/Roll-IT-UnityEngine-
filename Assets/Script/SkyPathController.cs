77c561951f900a2a039325d0cb18dda7e88fd12f
Davinderpal Cheema <dcheema6@uwo.ca>
2019-09-25 14:07:45 -0400

    init

---
+++ Assets/Script/SkyPathController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyPathController : MonoBehaviour
+{
+    public Vector3 [] positions;
+    public float speed;
+
+    private uint ind;
+    private Boolean forward;
+
+    void Start ()
+    {
+        forward = true;
+        ind = 0;
+        positions[ind] = transform.localPosition;
+    }
+
+    void FixedUpdate ()
+    {
+        if (ind == 0) forward = true;
+        else if (ind == positions.Length - 1) forward = false;
+
+        uint nxtInd = ind + 1;
+        if (!forward) nxtInd = ind - 1;
+
+        if (Vector3.Distance(transform.localPosition, positions[nxtInd]) > 0.5)
+        {
+            Vector3 diff = positions[nxtInd] - positions[ind];;
+
+            if (diff.magnitude < speed) transform.localPosition = positions[nxtInd];
+            else
+            {
+                transform.position += diff.normalized * speed;
+            }
+        }
+        else
+        {
+            ind = nxtInd;
+        }
+    }
+}
